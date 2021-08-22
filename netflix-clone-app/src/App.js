5025b4791d7832bb2706196a4a9fc53ff95f38bf
Abhishek-Raj-Poudel <89299685+Abhishek-Raj-Poudel@users.noreply.github.com>
2021-08-22 07:38:01 +0545

    First commit
    Netfix clone

---
+++ netflix-clone-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Row from './Row';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hey Lets make Netflix Clone !!!</h1>
+      <Row title="NETFLIX ORIGINAL" />
+      <Row title="Trending" />
+    </div>
+  );
+}
+
+export default App;
